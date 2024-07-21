666b124c2cfecce0e4e886318deb807ed7bb8ca4
ssh8560 <ssh8560@naver.com>
2024-07-21 23:45:14 +0900

    preparing.....

--- src/pages/EditArticle.tsx
+++ src/pages/EditArticle.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import Input from '../components/ui/Input';
 import DefaultLayout from '../components/layout/DefaultLayout';
@@ -6,14 +6,19 @@ import TextArea from '../components/ui/TextArea';
 import EditArticleHeader from '../components/header/EditArticleHeader';
 
 const EditArticle = () => {
-  const handlePressSave = () => {};
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+
+  const handlePressSave = () => {
+    console.log(title, content);
+  };
 
   return (
     <DefaultLayout>
       <EditArticleHeader title={'글 작성하기'} onPressSave={handlePressSave} />
       <View style={{padding: 16, gap: 16, flex: 1}}>
-        <Input label="제목" />
-        <TextArea label="내용" />
+        <Input label="제목" value={title} onChange={setTitle} />
+        <TextArea label="내용" value={content} onChange={setContent} />
       </View>
     </DefaultLayout>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DefaultLayout from '../components/layout/DefaultLayout';
 import Button from '../components/ui/Button';
 import Input from '../components/ui/Input';
@@ -8,6 +8,9 @@ import BackHeader from '../components/header/BackHeader';
 interface LoginProps {}
 
 const Login = ({navigation}: LoginProps) => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   const handlePressLogin = () => {};
   const handlePressJoin = () => {
     navigation.navigate('Join');
@@ -17,8 +20,13 @@ const Login = ({navigation}: LoginProps) => {
     <DefaultLayout>
       <BackHeader title="로그인" />
       <View style={{flex: 1, padding: 16, justifyContent: 'center', gap: 16}}>
-        <Input label="이메일" />
-        <Input label="비밀번호" />
+        <Input label="이메일" value={email} onChange={setEmail} />
+        <Input
+          label="비밀번호"
+          value={password}
+          onChange={setPassword}
+          secret
+        />
         <Button text="로그인" onPress={handlePressLogin} />
         <Button text="회원가입" onPress={handlePressJoin} />
       </View>
