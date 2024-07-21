a772dda97c61056e3a758e52ac44dc11dd82b137
ssh8560 <ssh8560@naver.com>
2024-07-21 23:21:07 +0900

    preparing.....

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import DefaultLayout from '../components/layout/DefaultLayout';
 import BackHeader from '../components/header/BackHeader';
@@ -8,16 +8,22 @@ import Button from '../components/ui/Button';
 interface JoinProps {}
 
 const Join = ({}: JoinProps) => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   const handlePressJoin = () => {};
 
   return (
     <DefaultLayout>
       <BackHeader title="회원가입" />
       <View style={{padding: 16, gap: 16}}>
-        <Input label="이메일" />
-        <Input label="비밀번호" secret />
-        <Input label="비밀번호 확인" secret />
-        <Input label="닉네임" />
+        <Input label="이메일" value={email} onChange={setEmail} />
+        <Input
+          label="비밀번호"
+          value={password}
+          onChange={setPassword}
+          secret
+        />
         <Button text="가입하기" onPress={handlePressJoin} />
       </View>
     </DefaultLayout>
